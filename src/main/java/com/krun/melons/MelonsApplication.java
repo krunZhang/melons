40c2c318fc548b1868c7494c80596cabb8cd1783
krunZhang <37580774+krunZhang@users.noreply.github.com>
2018-05-29 16:26:06 +0800

    提取 "启用状态查询" 到 MelonsEnableService 中 更新 MelonsApplication 注释

--- src/main/java/com/krun/melons/MelonsApplication.java
+++ src/main/java/com/krun/melons/MelonsApplication.java
@@ -1,8 +1,20 @@
+/*
+ * Copyright © 2018 krun, All Rights Reserved.
+ * Project: melons
+ * File:      MelonsApplication.java
+ * Date:    18-5-29 下午4:26
+ * Author: krun
+ */
+
 package com.krun.melons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author krun
+ * @date 2018/05/29
+ */
 @SpringBootApplication
 public class MelonsApplication {
 
--- src/main/java/com/krun/melons/commons/service/MelonsEnableService.java
+++ src/main/java/com/krun/melons/commons/service/MelonsEnableService.java
@@ -2,7 +2,7 @@
  * Copyright © 2018 krun, All Rights Reserved.
  * Project: melons
  * File:      MelonsEnableService.java
- * Date:    18-5-29 下午3:12
+ * Date:    18-5-29 下午4:26
  * Author: krun
  */
 
@@ -10,6 +10,11 @@ package com.krun.melons.commons.service;
 
 import com.krun.melons.commons.entity.EnableField;
 import com.krun.melons.commons.repository.MelonsEnableRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
 
 /**
  * 对应 {@link MelonsEnableRepository } 的服务接口
@@ -20,4 +25,47 @@ import com.krun.melons.commons.repository.MelonsEnableRepository;
  * @date 2018/05/29
  */
 public interface MelonsEnableService<T extends EnableField, R extends MelonsEnableRepository<T>>
-		extends MelonsUUIDService<T, R> {}
+		extends MelonsUUIDService<T, R> {
+
+	/**
+	 * 获取关于启用状态的查询条件
+	 * @param enable 启用状态
+	 * @return 构造好的查询条件
+	 */
+	default Specification<T> getEnableSpecification(Boolean enable) {
+		return (root, query, cb) -> cb.equal(root.get("enable").as(Boolean.class), enable);
+	}
+
+	/**
+	 * 查询所有启用的实体
+	 * @return 启用的所有实体
+	 */
+	default List<T> findAllEnable () {
+		return getRepository().findAll(getEnableSpecification(true));
+	}
+
+	/**
+	 * 查询所有禁用的实体
+	 * @return 禁用的所有实体
+	 */
+	default List<T> findAllNotEnable() {
+		return getRepository().findAll(getEnableSpecification(false));
+	}
+
+	/**
+	 * 查询所有启用的实体
+	 * @return 启用的所有实体
+	 */
+	default Page<T> findAllEnable (Pageable pageable) {
+		return findAll(getEnableSpecification(true), pageable);
+	}
+
+	/**
+	 * 分页查询所有禁用的实体
+	 * @return 禁用的所有实体
+	 */
+	default Page<T> findAllNotEnable(Pageable pageable) {
+		return findAll(getEnableSpecification(false), pageable);
+	}
+
+}
