4d02d1ac1368bdc903022cd9f54077352a636761
krunZhang <37580774+krunZhang@users.noreply.github.com>
2018-05-29 15:10:44 +0800

    追加一些对应基础实体字段的服务默认方法实现

---
+++ src/main/java/com/krun/melons/service/PermissionService.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2018 krun, All Rights Reserved.
+ * Project: melons
+ * File:      PermissionService.java
+ * Date:    18-5-29 下午3:10
+ * Author: krun
+ */
+
+package com.krun.melons.service;
+
+import com.krun.melons.commons.service.MelonsService;
+
+/**
+ * 权限服务接口
+ *
+ * @author krun
+ * @date 2018/05/29
+ */
+public interface PermissionService extends MelonsService {}
