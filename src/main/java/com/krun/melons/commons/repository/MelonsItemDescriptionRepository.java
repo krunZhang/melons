1663e216b163011db6ee528bedb28a06b17f5901
krunZhang <37580774+krunZhang@users.noreply.github.com>
2018-05-29 11:34:31 +0800

    更新一下注释

--- src/main/java/com/krun/melons/commons/repository/MelonsItemDescriptionRepository.java
+++ src/main/java/com/krun/melons/commons/repository/MelonsItemDescriptionRepository.java
@@ -2,7 +2,7 @@
  * Copyright © 2018 krun, All Rights Reserved.
  * Project: melons
  * File:      MelonsItemDescriptionRepository.java
- * Date:    18-5-29 上午11:29
+ * Date:    18-5-29 上午11:34
  * Author: krun
  */
 
@@ -23,6 +23,12 @@ import java.util.Optional;
 @NoRepositoryBean
 public interface MelonsItemDescriptionRepository<T extends ItemDescriptionFields> extends MelonsEnableRepository<T> {
 
+	/**
+	 * 根据条件查询实体
+	 * @param name 实体名称字段值
+	 * @param enable 实体启用状态
+	 * @return 符合条件的实体，用 {@link Optional} 包裹，值可能为空
+	 */
 	Optional<T> findByNameAndEnable(String name, Boolean enable);
 
 }
