b5de6c547f67a1eb2fdd7cdbd1f71da441947d61
krunZhang <37580774+krunZhang@users.noreply.github.com>
2018-05-29 11:04:20 +0800

    添加一个检查 List 是否为空 (list != null && list.size() != 0) 的判定式

--- src/main/java/com/krun/melons/commons/Commons.java
+++ src/main/java/com/krun/melons/commons/Commons.java
@@ -2,15 +2,22 @@
  * Copyright © 2018 krun, All Rights Reserved.
  * Project: melons
  * File:      Commons.java
- * Date:    18-5-29 上午10:19
+ * Date:    18-5-29 上午11:04
  * Author: krun
  */
 
 package com.krun.melons.commons;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * 一些暂未分类的帮助方法都先放在这里
  * @author krun
  * @date 2018/05/29
  */
-public class Commons {}
+public class Commons {
+
+	public static final Predicate<List> IS_LIST_EMPTY = list -> list == null || list.size() == 0;
+
+}
