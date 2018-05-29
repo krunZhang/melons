07a997416756a8c1415237d6f1661da2ef8dba2a
krunZhang <37580774+krunZhang@users.noreply.github.com>
2018-05-29 09:57:06 +0800

    Initial commit

---
+++ src/main/java/com/krun/melons/MelonsApplication.java
@@ -0,0 +1,12 @@
+package com.krun.melons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelonsApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(MelonsApplication.class, args);
+	}
+}
